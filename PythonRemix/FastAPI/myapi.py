15088ae21540419fe57baa5b0c7bf1753422260c
xavier <xavierfranzings@gmail.com>
2026-03-27 16:56:04 -0700

    inital commit with all files

---
+++ PythonRemix/FastAPI/myapi.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+# fast api instance
+app = FastAPI(
+    title="My firdt Data API",
+    description="A Simple API to demonstrate features for data applications",
+    version="0.0.1"
+)
+
+# API Documentation Concepts
